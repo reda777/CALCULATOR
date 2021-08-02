500b92f6581cc92477ca3b87e4fd290f0367caed
reda777 <redazara1998@gmail.com>
2021-08-02 15:56:12 +0100

    calculator works for a signle operation

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const buttons=document.querySelectorAll('button');
 const display=document.querySelector('.display span');
+let inputes=[];
+let operator;
 buttons.forEach(button=>{
     button.addEventListener('click',(e)=>{
         if(e.target.value==='clear')
-            display.textContent="";
-        else
-            display.textContent+=e.target.value;
+            clear(e.target.value);
+        if(['*','+','-','/'].includes(userInput)){
+            if(inputes.length!=0)
+            
+        }
+        populate(e.target.value);
     });
 });
+function clear(userInput){
+    display.textContent="";
+    inputes.length=0;
+    operator=undefined;
+}
+function populate(userInput){
+    if(userInput==='='){
+        inputes.push(display.textContent);
+        console.log(display.textContent);
+        result=operate(operator,inputes[0],inputes[1]);
+        display.textContent=result;
+    }
+    else if(['*','+','-','/'].includes(userInput)){
+        inputes.push(display.textContent);
+        console.log(display.textContent);
+        operator=userInput;
+        display.textContent="";
+    }
+    else{
+        display.textContent+=userInput;
+    }
+}
 function add(a,b){
     return a+b;
 }
